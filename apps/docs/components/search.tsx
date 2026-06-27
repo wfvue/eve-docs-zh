e10132aebae17435efde15758496818d600c6b1c
wfvue <47890437+wfvue@users.noreply.github.com>
2026-06-27 21:12:59 +0800

    fix: 添加静态搜索弹窗

---
+++ apps/docs/components/search.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { create } from '@orama/orama';
+import { useDocsSearch } from 'fumadocs-core/search/client';
+import { oramaStaticClient } from 'fumadocs-core/search/client/orama-static';
+import {
+  SearchDialog,
+  SearchDialogClose,
+  SearchDialogContent,
+  SearchDialogHeader,
+  SearchDialogIcon,
+  SearchDialogInput,
+  SearchDialogList,
+  SearchDialogOverlay,
+  type SharedProps,
+} from 'fumadocs-ui/components/dialog/search';
+
+function initOrama() {
+  return create({
+    schema: { _: 'string' },
+  });
+}
+
+export default function DefaultSearchDialog(props: SharedProps) {
+  const { search, setSearch, query } = useDocsSearch({
+    client: oramaStaticClient({
+      initOrama,
+    }),
+  });
+
+  return (
+    <SearchDialog search={search} onSearchChange={setSearch} isLoading={query.isLoading} {...props}>
+      <SearchDialogOverlay />
+      <SearchDialogContent>
+        <SearchDialogHeader>
+          <SearchDialogIcon />
+          <SearchDialogInput />
+          <SearchDialogClose />
+        </SearchDialogHeader>
+        <SearchDialogList items={query.data !== 'empty' ? query.data : null} />
+      </SearchDialogContent>
+    </SearchDialog>
+  );
+}
